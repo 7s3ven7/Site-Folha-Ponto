b1c860b787b8ccbbdcdb8f010c3a8f3f963dcf52
7s3ven7 <7s3ven7.1@gmail.com>
2026-02-01 19:47:35 -0300

    leitura de arquivos completa e validacao de caminhos para arquivos

--- Src/Csv.php
+++ Src/Csv.php
@@ -6,22 +6,94 @@ class Csv
 {
 
     private string $filename;
+    private array $path = [];
+    private string $fullPath;
 
-    public function __construct($filename)
+
+    public function __construct(string $filename, array $path = [])
     {
 
         $this->filename = $filename;
 
-    }
-
-    public function writeCSV(array $data): void
-    {
+        $this->path = $path;
 
     }
 
-    public function createCSV(array $data): void
+    public function writeCSV(array $data, bool $read): mixed
     {
-        
+
+        if ($read) {
+            $file = $this->readCSV($this->filename);
+        } else {
+            $file = $this->createCSV($data);
+        }
+
+        return $file;
+
+    }
+
+    private function testPath(): string
+    {
+
+        $fullPath = '';
+
+        foreach ($this->path as $path) {
+            $fullPath .= $path;
+
+            if (!is_dir($fullPath)) {
+                mkdir($fullPath);
+            }
+
+            $fullPath .= '\\';
+
+        }
+        $this->fullPath = $fullPath;
+
+        return $fullPath;
+
+    }
+
+    private function createCSV(array $data): mixed
+    {
+
+        $fullPath = $this->testPath();
+
+        //numero incremental para não tentar criar arquivo existente, e modificar filename caso precise
+        $i = 1;
+        while (file_exists($fullPath . $this->filename . '(' . $i . ').csv')) {
+            $i++;
+        }
+
+        return fopen($fullPath . $this->filename . '(' . $i . ').csv', 'w+');
+
+    }
+
+    private function readCSV(): mixed
+    {
+
+        $fullPath = $this->testPath();
+
+        if (file_exists($fullPath . $this->filename . '.csv')) {
+            return $this->transformCSV(fopen($fullPath . $this->filename . '.csv', 'r'));
+        }
+
+        return ['error' => 'file not found'];
+
+    }
+
+    private function transformCSV(mixed $file): mixed
+    {
+
+        //$map = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n','o','p','q','r','s', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+        $lines = [];
+
+        while (($line = fgetcsv($file)) !== false) {
+            $lines[] = $line;
+
+        }
+
+        return $lines;
     }
 
 }
--- index.php
+++ index.php
@@ -1,16 +1,22 @@
 <?php
 
 require_once('Src/Server.php');
+require_once('Src/Csv.php');
 
 use Src\Server;
+use Src\Csv;
 
 $server = new Server();
+$body = json_decode(file_get_contents('php://input'), true);
 
-switch ($this->method) {
+$csv = new CSV($body['filename'], $body['path']);
+switch ($server->getMethod()) {
     case 'GET':
+        $fileCsv = $csv->writeCSV([], $body['read']);
+        $server->response(201, $fileCsv);
         break;
     case 'POST':
         break;
     default:
-        $server->response(404, ['error', 'not allowed this method']);
+        $server->response(404, ['error' => 'not allowed this method']);
 }
